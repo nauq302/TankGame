a142ac8238e6d54ed693d68222bf321350d8531a
nauq302 <nauq302@gmail.com>
2020-09-01 14:11:27 +0700

    [Add] Hello World Commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by nauq302 on 01/09/2020.
+//
+
+#include <iostream>
+
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello World" <<'\n';
+}
